5ce40697847dc15a400e89b22fa98dc77a9cb04f
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-12-02 13:26:15 -0500

    Solved part 1 of Day 2: Cube Conundrum

---
+++ 2023/day_2.py
@@ -0,0 +1,47 @@
+def get_number(curr_line: str):
+    number = curr_line.strip()
+    if not number[-1].isdigit():
+        number = number[:-1]
+    return int(number)
+
+
+def get_color(curr_line: str):
+    color = curr_line.strip()
+    if not color[-1].isalpha():
+        color = color[:-1]
+    return color
+
+
+with open('input.txt', 'r') as file:
+    total = 0
+    line = file.readline()
+
+    while line:
+        games = {}
+        id = get_number(line[5:8])
+        i = len(str(id)) + 7
+        while i < len(line):
+            if line[i].isdigit():
+                number = get_number(line[i:i+3])
+                curr_index = len(str(number)) + i + 1
+                color = get_color(line[curr_index:curr_index+5])
+
+                if color not in games:
+                    games[color] = number
+                else:
+                    games[color] = max(games[color], number)
+
+                i = curr_index + len(color)
+
+            else:
+                i += 1
+
+        red = games['red'] - 12
+        green = games['green'] - 13
+        blue = games['blue'] - 14
+
+        if red < 1 and green < 1 and blue < 1:
+            total += id
+
+        line = file.readline()
+    print(total)
