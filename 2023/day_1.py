6c0adc545cb444d6b36d0c64686bc24f89f53f9c
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-12-01 22:59:37 -0500

    solved part 2 of Trebuchet?!

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -1,6 +1,8 @@
 
 with open('input.txt', 'r') as file:
     total_sum = 0
+    nums = ['one', 'two', 'three', 'four',
+            'five', 'six', 'seven', 'eight', 'nine']
     line = file.readline()
 
     while line:
@@ -9,15 +11,44 @@ with open('input.txt', 'r') as file:
 
         while l < len(line) - 2:
             if line[l].isdigit():
+                l = line[l]
                 break
+
+            if line[l:l+3] in nums:
+                l = str(nums.index(line[l:l+3]) + 1)
+                break
+
+            if line[l:l+4] in nums:
+                l = str(nums.index(line[l:l+4]) + 1)
+                break
+
+            if line[l:l+5] in nums:
+                l = str(nums.index(line[l:l+5]) + 1)
+                break
+
             l += 1
 
         while r > 0:
             if line[r].isdigit():
+                r = line[r]
                 break
+
+            if line[r-2:r+1] in nums:
+                r = str(nums.index(line[r-2:r+1]) + 1)
+                break
+
+            if line[r-3:r+1] in nums:
+                r = str(nums.index(line[r-3:r+1]) + 1)
+                break
+
+            if line[r-4:r+1] in nums:
+                r = str(nums.index(line[r-4:r+1]) + 1)
+                break
+
             r -= 1
 
-        total_sum += int(line[l] + line[r])
+        print(r)
+        total_sum += int(l + r)
         line = file.readline()
 
     print(total_sum)
