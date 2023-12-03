89b46540a26eb9ebf65d0e0a4d985ca3395aaad4
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-12-03 11:41:28 -0500

    adding part 1 code to day 1 and day 2

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -1,53 +1,83 @@
 
-with open('input.txt', 'r') as file:
-    total_sum = 0
-    nums = ['one', 'two', 'three', 'four',
-            'five', 'six', 'seven', 'eight', 'nine']
-    line = file.readline()
-
-    while line:
-        l = 0
-        r = len(line) - 1
-
-        while l < len(line) - 1:
-            if line[l].isdigit():
-                l = line[l]
-                break
-
-            if line[l:l+3] in nums:
-                l = str(nums.index(line[l:l+3]) + 1)
-                break
-
-            if line[l:l+4] in nums:
-                l = str(nums.index(line[l:l+4]) + 1)
-                break
-
-            if line[l:l+5] in nums:
-                l = str(nums.index(line[l:l+5]) + 1)
-                break
-
-            l += 1
-
-        while r >= 0:
-            if line[r].isdigit():
-                r = line[r]
-                break
-
-            if line[r-2:r+1] in nums:
-                r = str(nums.index(line[r-2:r+1]) + 1)
-                break
-
-            if line[r-3:r+1] in nums:
-                r = str(nums.index(line[r-3:r+1]) + 1)
-                break
-
-            if line[r-4:r+1] in nums:
-                r = str(nums.index(line[r-4:r+1]) + 1)
-                break
-
-            r -= 1
-
-        total_sum += int(l + r)
+def part_1():
+    with open('input.txt', 'r') as file:
+        total_sum = 0
         line = file.readline()
 
-    print(total_sum)
+        while line:
+            l = 0
+            r = len(line) - 1
+
+            while l < len(line) - 1:
+                if line[l].isdigit():
+                    break
+                l += 1
+
+            while r >= 0:
+                if line[r].isdigit():
+                    break
+                r -= 1
+
+            total_sum += int(line[l] + line[r])
+            line = file.readline()
+
+        print(total_sum)
+
+
+def part_2():
+    with open('input.txt', 'r') as file:
+        total_sum = 0
+        nums = ['one', 'two', 'three', 'four',
+                'five', 'six', 'seven', 'eight', 'nine']
+        line = file.readline()
+
+        while line:
+            l = 0
+            r = len(line) - 1
+
+            while l < len(line) - 1:
+                if line[l].isdigit():
+                    l = line[l]
+                    break
+
+                if line[l:l+3] in nums:
+                    l = str(nums.index(line[l:l+3]) + 1)
+                    break
+
+                if line[l:l+4] in nums:
+                    l = str(nums.index(line[l:l+4]) + 1)
+                    break
+
+                if line[l:l+5] in nums:
+                    l = str(nums.index(line[l:l+5]) + 1)
+                    break
+
+                l += 1
+
+            while r >= 0:
+                if line[r].isdigit():
+                    r = line[r]
+                    break
+
+                if line[r-2:r+1] in nums:
+                    r = str(nums.index(line[r-2:r+1]) + 1)
+                    break
+
+                if line[r-3:r+1] in nums:
+                    r = str(nums.index(line[r-3:r+1]) + 1)
+                    break
+
+                if line[r-4:r+1] in nums:
+                    r = str(nums.index(line[r-4:r+1]) + 1)
+                    break
+
+                r -= 1
+
+            total_sum += int(l + r)
+            line = file.readline()
+
+        print(total_sum)
+
+
+part_1()
+part_2()
--- 2023/day_2.py
+++ 2023/day_2.py
@@ -12,31 +12,72 @@ def get_color(curr_line: str):
     return color
 
 
-with open('input.txt', 'r') as file:
-    total = 0
-    line = file.readline()
-
-    while line:
-        games = {}
-        id = get_number(line[5:8])
-        i = len(str(id)) + 7
-        while i < len(line):
-            if line[i].isdigit():
-                number = get_number(line[i:i+3])
-                curr_index = len(str(number)) + i + 1
-                color = get_color(line[curr_index:curr_index+5])
-
-                if color not in games:
-                    games[color] = number
-                else:
-                    games[color] = max(games[color], number)
-
-                i = curr_index + len(color)
-
-            else:
-                i += 1
-
-        total += games['red'] * games['green'] * games['blue']
-
+def part_1():
+    with open('input.txt', 'r') as file:
+        total = 0
         line = file.readline()
-    print(total)
+
+        while line:
+            games = {}
+            id = get_number(line[5:8])
+            i = len(str(id)) + 7
+            while i < len(line):
+                if line[i].isdigit():
+                    number = get_number(line[i:i+3])
+                    curr_index = len(str(number)) + i + 1
+                    color = get_color(line[curr_index:curr_index+5])
+
+                    if color not in games:
+                        games[color] = number
+                    else:
+                        games[color] = max(games[color], number)
+
+                    i = curr_index + len(color)
+
+                else:
+                    i += 1
+
+            red = games['red'] - 12
+            green = games['green'] - 13
+            blue = games['blue'] - 14
+
+            if red < 1 and green < 1 and blue < 1:
+                total += id
+
+            line = file.readline()
+        print(total)
+
+
+def part_2():
+    with open('input.txt', 'r') as file:
+        total = 0
+        line = file.readline()
+
+        while line:
+            games = {}
+            id = get_number(line[5:8])
+            i = len(str(id)) + 7
+            while i < len(line):
+                if line[i].isdigit():
+                    number = get_number(line[i:i+3])
+                    curr_index = len(str(number)) + i + 1
+                    color = get_color(line[curr_index:curr_index+5])
+
+                    if color not in games:
+                        games[color] = number
+                    else:
+                        games[color] = max(games[color], number)
+
+                    i = curr_index + len(color)
+
+                else:
+                    i += 1
+
+            total += games['red'] * games['green'] * games['blue']
+
+            line = file.readline()
+        print(total)
+
+
+part_1()
+part_2()
