d242cbbb74fccf40c027590ae9647f8add548b77
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-12-06 14:21:58 -0500

    refactored part 1 of day 6 solution

--- 2023/day_6.py
+++ 2023/day_6.py
@@ -8,27 +8,16 @@ def calculate_min_hold_time(time, distance):
 def part_a():
     with open('input.txt', 'r') as file:
         input = file.read().splitlines()
-        times = input[0].split(': ')[1].strip().split(' ')
-        distances = input[1].split(': ')[1].strip().split(' ')
+        times = list(map(int, input[0].split(': ')[1].strip().split()))
+        distances = list(map(int, input[1].split(': ')[1].strip().split()))
         total_wins = 1
- 
-        while times and distances:
-            if not times[0].isdigit():
-                times.pop(0)
 
-            if not distances[0].isdigit():
-                distances.pop(0)
-
-            if times[0].isdigit() and distances[0].isdigit():
-                curr_time = int(times.pop(0))
-                curr_distance = int(distances.pop(0))
-                min_hold_time = calculate_min_hold_time(curr_time, curr_distance)
-                max_hold_time = curr_time - min_hold_time
-                total_wins *=  max_hold_time - min_hold_time + 1
+        for i in range(len(times)):
+            min_hold_time = calculate_min_hold_time(times[i], distances[i])
+            max_hold_time = times[i] - min_hold_time
+            total_wins *=  max_hold_time - min_hold_time + 1
         
         print(total_wins)
-
-
 
 
 
