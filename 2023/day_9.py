cf612a32574f87b98c80bc81e661514b8a1fd009
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-12-09 12:27:39 -0500

    Solved part two of Day 9: Mirage Maintenance

--- 2023/day_9.py
+++ 2023/day_9.py
@@ -30,3 +30,38 @@ def part_a():
         print(total_sum)
 
 part_a()
+
+def part_b():
+    with open('input.txt', 'r') as file:
+        input = file.read().splitlines()
+        total_sum = 0
+
+        for line in input:
+            line = list(map(int, line.split()))
+            matrix = []
+            matrix.append(line)
+
+            while True:
+                row = []
+                ctr = 0
+
+                for idx in range(len(line) - 1):
+                    row.append(line[idx + 1] - line[idx])
+                    if line[idx + 1] - line[idx] == 0:
+                        ctr += 1
+
+                line = row
+                matrix.append(row)
+
+                if ctr == len(row):
+                    break
+            
+            curr_sum = 0
+            for row in range(len(matrix)-2, -1, -1):
+                curr_sum = matrix[row][0] - curr_sum
+
+            total_sum += curr_sum
+            
+        print(total_sum) 
+
+part_b()
