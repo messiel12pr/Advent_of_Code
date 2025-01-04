cb5e16320c3ffac450ece10124df0f04b824ffcb
Joel M. Gonzalez <joelm12pr@gmail.com>
2025-01-04 16:22:58 -0500

    Revert "solved part 1 of day 7"
    This reverts commit 114afbc4af754b50828016e4ac990c5dcf97a640.

--- 2024/day_7.py
+++ 2024/day_7.py
@@ -22,28 +22,4 @@ def part_1():
 
     print(res)
 
-
-def evaluate2(val, nums, curr):
-    if not nums and curr == val:
-        return True
-
-    if not nums or curr > val:
-        return False
-
-    concatenated = str(nums[0]) + str(curr)
-
-    return evaluate(val, nums[1:], nums[0] * curr) or evaluate(val, nums[1:], nums[0] + curr) or evaluate(val, nums[1:], concatenated)
-
-def part_2():
-    res = 0
-    for line in data:
-        val, nums = line.split(':')
-        val = int(val)
-        nums = list(map(int, nums.split()))
-        
-        if evaluate2(val, nums, 0):
-            res += val
-
-    print(res)
-
 part_1()
