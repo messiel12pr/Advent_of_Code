6e73640625bc5b3808b5489afae22f789ac8f79f
Joel M <joel.gonzalez35@upr.edu>
2025-12-04 15:50:32 -0500

    Solved part 1 and 2 of day 2

--- 2025/day_2.py
+++ 2025/day_2.py
@@ -2,7 +2,33 @@ import sys
 
 def part_1():
     test_cases = sys.stdin.readline().strip().split(',')
+    res = 0
 
-    # incomplete
     for case in test_cases:
         lower, upper = map(int, case.split('-'))
+        for num in range(lower, upper + 1):
+            str_num = str(num)
+            if str_num[:(len(str_num)//2)] * 2 == str_num:
+                res += num
+
+    return res
+
+
+def part_2():
+    test_cases = sys.stdin.readline().strip().split(',')
+    res = 0
+
+    for case in test_cases:
+        lower, upper = map(int, case.split('-'))
+        repeated = set()
+
+        for num in range(lower, upper + 1):
+            str_num = str(num)
+            for i in range(1, (len(str_num)//2) + 1):
+                if str_num[:i] * (len(str_num)//i) == str_num and num not in repeated:
+                    res += num
+                    repeated.add(num)
+    return res
+
+#print(part_1())
+print(part_2())
